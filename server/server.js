220420ca4eec61b6d75e967c328e10448ab3b3de
Labeud <bedos.alexandre@gmail.com>
2018-11-26 17:27:25 +0100

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -52,6 +52,25 @@ app.get("/todos/:id", (req, res) => {
     })
 })
 
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndDelete(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({todo})
+    })
+    .catch((e) => {
+      res.status(400).send()
+    })
+})
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
 })
